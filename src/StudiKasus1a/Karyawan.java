edd9daf1c7a904b1ee09fc7807372613b06dccc1
idhamma <idhammakbar@gmail.com>
2023-03-07 08:28:39 +0700

    Test 01

--- src/StudiKasus1a/Karyawan.java
+++ src/StudiKasus1a/Karyawan.java
@@ -1,4 +1,21 @@
 package StudiKasus1a;
 
 public class Karyawan {
+
+    private String nama;
+    private String alamat;
+    private String noTelp;
+    private String jk;
+    private String Kategori;
+    Karyawan(){
+
+    }
+
+    public Karyawan(String nama, String alamat, String noTelp, String jk, String kategori) {
+        this.nama = nama;
+        this.alamat = alamat;
+        this.noTelp = noTelp;
+        this.jk = jk;
+        Kategori = kategori;
+    }
 }
